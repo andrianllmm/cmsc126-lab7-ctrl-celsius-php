b5bbeb447a64dc433edef90f8646b715587e1ac4
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-04-24 16:22:10 +0800

    Create 404 page

---
+++ views/errors/404.php
@@ -0,0 +1,17 @@
+<?php require BASE_PATH . '/views/layout/header.php'; ?>
+
+<div class="m-8 flex flex-col items-center justify-center gap-4">
+
+    <h1 class="text-3xl font-bold text-center">
+        404 Not Found
+    </h1>
+
+    <a href="/">
+        <button class="bg-red-800 text-white px-4 py-2">
+            Go Back
+        </button>
+    </a>
+
+</div>
+
+<?php require BASE_PATH . '/views/layout/footer.php'; ?>
