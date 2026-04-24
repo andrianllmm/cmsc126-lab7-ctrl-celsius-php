4504a6331e1238984128cd111970f4b6b5363b1f
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-04-24 16:11:31 +0800

    Create database seed

---
+++ database/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO courses (course_name) VALUES
+('BS Computer Science'),
+('BS Applied Mathematics'),
+('BS Statistics');
+
+INSERT INTO students (name, age, email, course_id, year_level, graduation_status, image_path)
+VALUES
+('Juan Dela Cruz', 21, 'juan@example.com', 1, 3, 0, 'assets/uploads/sample.jpg');
