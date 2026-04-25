1681d6948e05bb388891656a58d15e2c6bf804ef
Jemarco Briz <jemarcobriz123@gmail.com>
2026-04-25 20:01:04 +0800

    fix: resolve delimeter issue in the trigger and script an make it idempotent by dropping first before creating

--- database/trigger.sql
+++ database/trigger.sql
@@ -1,16 +1,25 @@
 USE cmsc126_db;
 
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS generate_student_id $$
+
 CREATE TRIGGER generate_student_id
 BEFORE INSERT ON students
 FOR EACH ROW
 BEGIN
-    DECLARE next_id    INT;
+    DECLARE next_id INT;
     DECLARE start_year INT;
 
-    -- Atomically grab and increment the counter
+    -- Increment sequence
     UPDATE student_id_seq SET next_val = next_val + 1;
+
+    -- Get the value we just reserved
     SELECT next_val - 1 INTO next_id FROM student_id_seq;
 
-    SET start_year = YEAR(CURDATE()) - (NEW.year_level);
+    -- Compute student ID
+    SET start_year = YEAR(CURDATE()) - NEW.year_level;
     SET NEW.student_id = CONCAT(start_year, '-', LPAD(next_id, 5, '0'));
-END
+END$$
+
+DELIMITER ;
